6eb86469b90af4ba09be7608c7cc6b8f11566a80
Milind Sharma <milindsharma8@gmail.com>
2021-10-11 14:11:52 +0800

    Make function for stock weight calculations

--- portfolio_allocation.py
+++ portfolio_allocation.py
@@ -1,14 +1,23 @@
 """Calculates the allocation of money for investment portfolio"""
 
+
+from typing import Dict
+
+
 def main():
     """Generates the ratios for the allocation of money"""
+    
+    weight_ratios = {
+        'OFSS' : 1.0,
+        'ITC' : 1.0,
+        'EBBETF0430' : 7.0,
+        'NETFMID150' : 12.4,
+        'GOLDBEES' : 14.0,
+        'JUNIORBEES' : 14.0,
+        'NIFTYBEES' : 14.0
+    }
 
-    assets = {'OFSS' : 0.025, 
-              'ITC' : 0.025, 
-              'EBBETF0430' : 0.140, 
-              'GOLDBEES' : 0.270, 
-              'NIFTYBEES' : 0.270, 
-              'JUNIORBEES' : 0.270}
+    percentage_weights: Dict[str, float] = get_percentage_weights(weight_ratios)
 
     while True:
         command : str = str(input('\nEnter command (q to quit, enter to calculate) :'))
@@ -32,13 +41,27 @@ def main():
                     total = float(total)
 
                 print(f'Total value = {total}')
-                for key in assets:
-                    fraction = assets[key]
+                for key in percentage_weights:
+                    fraction = percentage_weights[key]
                     allocation = fraction * total
                     print(f'\n{fraction * 100}% of {total} in {key} = {allocation}')
                     
             except ValueError as err:
                 print(f'{err}\nPlease enter a number!')
+
+
+def get_percentage_weights(weight_ratios: Dict[str, float]) -> Dict[str, float]:
+    percentage_weight_dict = {}
+    total = sum(weight_ratios.values())
+    unit_fraction = 1/total
+
+    for key in weight_ratios:
+        weight = weight_ratios[key]
+        weight_fraction = unit_fraction * weight
+        percentage_weight_dict.update({key: weight_fraction}) 
+    
+    return percentage_weight_dict
+
 
 if __name__ == '__main__':
     main()
