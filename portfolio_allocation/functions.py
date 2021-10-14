24f7656a53de352633764c9e99e43a0f9573b3d0
Milind Sharma <milindsharma8@gmail.com>
2021-10-14 21:11:53 +0800

    Create functions.py

---
+++ portfolio_allocation/functions.py
@@ -0,0 +1,56 @@
+
+from typing import Dict
+
+def get_percentage_weights(weight_ratios: Dict[str, float]) -> Dict[str, float]:
+    """Returns the fractional weightages of instruments
+
+    Args:
+        weight_ratios (Dict[str, float]): Weightage ratios that you set
+
+    Returns:
+        Dict[str, float]: Final fractional weightages
+    """
+    percentage_weight_dict = {}
+    total = sum(weight_ratios.values())
+    unit_fraction = 1/total
+
+    for key in weight_ratios:
+        weight = weight_ratios[key]
+        weight_fraction = unit_fraction * weight
+        percentage_weight_dict.update({key: weight_fraction}) 
+
+    return percentage_weight_dict
+
+
+def print_allocations(
+                    weights: Dict[str, float],
+                    total: float) -> None: 
+    """Prints the final division of funds
+
+    Args:
+        weights (Dict[str, float]): fractional weightages of instruments
+        total (float): total money to be invested
+    """
+    for key in weights:
+        fraction = weights[key]
+        allocation = fraction * total
+
+        print(f'\n{round((fraction * 100), 2)}% of {total} in {key} = {allocation}')
+
+
+def get_rtf_ratios() -> Dict[str, float]:
+    """Get the ratios from the rtf file in directory
+
+    Returns:
+        Dict[str, float]: Dictionary of stocks and ratios
+    """
+    return {'sample': 1.0}
+
+
+def get_excel_ratios() -> Dict[str, float]:
+    """Get the ratios from the excel file in the directory
+
+    Returns:
+        Dict[str, float]: Dictionary of stocks and ratios
+    """
+    return {'sample': 1.0}
