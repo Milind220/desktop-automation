75f4aeb797aa3d9c7f3787400e28a988d8573cf3
Milind Sharma <milindsharma8@gmail.com>
2021-11-20 12:48:09 +0800

    Add generate allocation figure func, minor changes

--- portfolio_allocation/functions.py
+++ portfolio_allocation/functions.py
@@ -2,6 +2,7 @@
 
 from typing import Dict, List
 
+import matplotlib.pyplot as plt
 import openpyxl
 
 
@@ -35,11 +36,11 @@ def print_allocations(
         weights (Dict[str, float]): fractional weightages of instruments
         total (float): total money to be invested
     """
-    for key in weights:
-        fraction = weights[key]
+    for ticker in weights:
+        fraction = weights[ticker]
         allocation = fraction * total
 
-        print(f'\n{round((fraction * 100), 2)}% of {total} in {key} = {allocation}')
+        print(f'\n{round((fraction * 100), 2)}% of {total} in {ticker} = {allocation}')
 
 
 def get_rtf_ratios() -> Dict[str, float]:
@@ -80,7 +81,9 @@ def get_excel_ratios() -> Dict[str, float]:
     """
     result_dict = {}
     try:
-        wb = openpyxl.load_workbook(filename='portfolio_allocation/ratios.xlsx', read_only=True)
+        wb = openpyxl.load_workbook(
+            filename = 'portfolio_allocation/ratios.xlsx',
+            read_only = True)
         ws = wb.active
         for row in ws.rows:
             result_dict.update({str(row[0].value): float(row[1].value)})
@@ -90,6 +93,18 @@ def get_excel_ratios() -> Dict[str, float]:
         print('Sorry, Excel file could not be read! Try the .rtf file...')
         return {'sample': 1.0}
 
-    
+
+def generate_allocation_figure(weights: Dict[str, float], total: float):
+    values: List[float] = list(weights.values())
+    labels: List[str] = []
+    for ticker in weights:
+        fraction: float = weights[ticker]
+        allocation: float = total * fraction
+        labels.append(f'{ticker}: {fraction*100}% = {allocation}')
+
+    plt.bar(range(len(weights)), values, tick_label = labels)
+    plt.show()
+
+
 if __name__ == '__main__':
     pass
