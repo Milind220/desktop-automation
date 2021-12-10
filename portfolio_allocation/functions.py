1c7e16c1c361de76fd42b9caad3c2e1fce929364
Milind Sharma <milindsharma8@gmail.com>
2021-12-11 00:07:55 +0800

    Refactor code with flake8 and black

--- portfolio_allocation/functions.py
+++ portfolio_allocation/functions.py
@@ -11,7 +11,8 @@ import openpyxl
 # TODO: format private and public functions in correct order.
 # TODO: Add print statement reminder to tell user to close the plot.
 
-def configure_logs(logfile_name: str = 'portfolio_allocation_errors.log') -> None:
+
+def configure_logs(logfile_name: str = "portfolio_allocation_errors.log") -> None:
     """Configures settings for the script log.
 
     Args:
@@ -19,13 +20,16 @@ def configure_logs(logfile_name: str = 'portfolio_allocation_errors.log') -> Non
             'portfolio_allocation_errors.log'.  
     """
     logging.basicConfig(
-        filename = logfile_name,
-        filemode = 'w',
-        level = logging.ERROR,
-        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        filename=logfile_name,
+        filemode="w",
+        level=logging.ERROR,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
-    print('\n\nStatus: Logs configured.'
-         f'\n\tLogs for this run can be found in {logfile_name}')
+    print(
+        "\n\nStatus: Logs configured."
+        f"\n\tLogs for this run can be found in {logfile_name}"
+    )
 
 
 def get_percentage_weights(weight_ratios: Dict[str, float]) -> Dict[str, float]:
@@ -39,19 +43,17 @@ def get_percentage_weights(weight_ratios: Dict[str, float]) -> Dict[str, float]:
     """
     percentage_weight_dict = {}
     total = sum(weight_ratios.values())
-    unit_fraction = 1/total
+    unit_fraction = 1 / total
 
     for key in weight_ratios:
         weight = weight_ratios[key]
         weight_fraction = unit_fraction * weight
-        percentage_weight_dict.update({key: weight_fraction}) 
+        percentage_weight_dict.update({key: weight_fraction})
 
     return percentage_weight_dict
 
 
-def _print_allocations(
-                    weights: Dict[str, float],
-                    total: float) -> None: 
+def _print_allocations(weights: Dict[str, float], total: float) -> None:
     """Prints the final division of funds.
 
     Args:
@@ -62,7 +64,7 @@ def _print_allocations(
         fraction = weights[ticker]
         allocation = fraction * total
 
-        print(f'\n{round((fraction * 100), 2)}% of {total} in {ticker} = {allocation}')
+        print(f"\n{round((fraction * 100), 2)}% of {total} in {ticker} = {allocation}")
 
 
 def get_rtf_ratios() -> Dict[str, float]:
@@ -73,28 +75,29 @@ def get_rtf_ratios() -> Dict[str, float]:
     """
     result_dict = {}
     try:
-        with open('ratios.rtf', 'r') as f:
+        with open("ratios.rtf", "r") as f:
             lines = f.readlines()
 
         for line in lines:
-            if line[0].isalpha():   # RTF files have a lot of random setting lines in the beginning.
-
-                line_list: List[str] = line.split(',')
+            # RTF files have a lot of random setting lines in the beginning.
+            if line[0].isalpha():
+                line_list: List[str] = line.split(",")
                 instrument: str = line_list[0]
-                for i, x in enumerate(line_list[1][::-1]): # Iterate backwards.
+
+                for i, x in enumerate(line_list[1][::-1]):  # Iterate backwards.
                     if x.isdigit():
                         break
-                    n = -i - 1 # find the index from the end where the float begins.
+                    n = -i - 1  # find the index from the end where the float begins.
                 ratio: float = float(line_list[1][:n])
 
-                result_dict.update({instrument: ratio}) 
+                result_dict.update({instrument: ratio})
         return result_dict
 
     except Exception:
-        print('Sorry, RTF file could not be read! Try the Excel file...')
-        _log_error(section = 'RTF file')
-        return {'sample': 1.0}
-         
+        print("Sorry, RTF file could not be read! Try the Excel file...")
+        _log_error(section="RTF file")
+        return {"sample": 1.0}
+
 
 def get_excel_ratios() -> Dict[str, float]:
     """Gets the ratios from the excel file in the directory.
@@ -104,37 +107,35 @@ def get_excel_ratios() -> Dict[str, float]:
     """
     result_dict = {}
     try:
-        wb = openpyxl.load_workbook(
-            filename = 'ratios.xlsx',
-            read_only = True)
+        wb = openpyxl.load_workbook(filename="ratios.xlsx", read_only=True)
         ws = wb.active
         for row in ws.rows:
             result_dict.update({str(row[0].value): float(row[1].value)})
         return result_dict
-                
+
     except Exception:
-        print('Sorry, Excel file could not be read! Try the .rtf file...')
-        _log_error(section = 'Excel file')
-        return {'sample': 1.0}
+        print("Sorry, Excel file could not be read! Try the .rtf file...")
+        _log_error(section="Excel file")
+        return {"sample": 1.0}
 
 
 def get_input_total():
     while True:
         try:
-            total = input('\nEnter amount here: ')
-            if '+' in total:
-                nums = total.split('+')
+            total = input("\nEnter amount here: ")
+            if "+" in total:
+                nums = total.split("+")
                 total = sum(map(float, nums))
-                
+
             total = float(total)
             break
 
         except ValueError:
-            print('Sorry\nPlease enter a number!')
-            _log_error('get_input_total')
+            print("Sorry\nPlease enter a number!")
+            _log_error("get_input_total")
             continue
 
-    return total 
+    return total
 
 
 def _generate_allocation_figure(weights: Dict[str, float], total: float):
@@ -143,29 +144,30 @@ def _generate_allocation_figure(weights: Dict[str, float], total: float):
     for ticker in weights:
         fraction: float = weights[ticker]
         allocation: float = total * fraction
-        labels.append(f'{ticker}: {fraction*100}% = {allocation}')
+        labels.append(f"{ticker}: {fraction*100}% = {allocation}")
 
-    plt.bar(range(len(weights)), values, tick_label = labels)
-    plt.tick_params(axis='x', labelsize = 6, rotation=20)
+    plt.bar(range(len(weights)), values, tick_label=labels)
+    plt.tick_params(axis="x", labelsize=6, rotation=20)
     plt.show()
 
 
 def _log_error(section: str) -> None:
-    logging.error(f'Scraping error: {section}\n'
-                  f'\tError: {sys.exc_info()[0]}\n')
+    logging.error(f"Scraping error: {section}\n" f"\tError: {sys.exc_info()[0]}\n")
 
 
 def show_output(weights: Dict[str, float], total: float):
-    print('See allocations in command line or graphical output')
-    choice = input('Enter 1 for command line, 2 for graphical output, anything else for both : ')
-    if choice == '1':
+    print("See allocations in command line or graphical output")
+    choice = input(
+        "Enter 1 for command line, 2 for graphical output, anything else for both : "
+    )
+    if choice == "1":
         _print_allocations(weights, total)
-    elif choice == '2':
+    elif choice == "2":
         _generate_allocation_figure(weights, total)
     else:
         _print_allocations(weights, total)
         _generate_allocation_figure(weights, total)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
--- portfolio_allocation/main.py
+++ portfolio_allocation/main.py
@@ -8,48 +8,47 @@ import functions
 
 def main():
     """Prints the ratios for the allocation of money"""
-
     functions.configure_logs()
-    print('\nWhere would you like to read stock weight ratios from?\n')
+    print("\nWhere would you like to read stock weight ratios from?\n")
 
-    while True:     # Ensures that only valid input is given to source
-        source = input('a for rtf file, b for excel file\nEnter here: ')
+    while True:  # Ensures that only valid input is given to source
+        source = input("a for rtf file, b for excel file\nEnter here: ")
 
-        if source.lower() in ('a', 'b'):
+        if source.lower() in ("a", "b"):
             break
         else:
-            print('Please enter a valid input!')
+            print("Please enter a valid input!")
             continue
-    
-    if source == 'a':
+
+    if source == "a":
         weight_ratios: Dict[str, float] = functions.get_rtf_ratios()
     else:
         weight_ratios: Dict[str, float] = functions.get_excel_ratios()
-    
+
     # To ensure that weights are in percentages, if not already.
-    percentage_weights: Dict[str, float] = functions.get_percentage_weights(weight_ratios) 
-
+    percentage_weights: Dict[str, float] = functions.get_percentage_weights(
+        weight_ratios
+    )
     while True:
-        command : str = str(input('\nEnter command (q to quit, enter to calculate) :'))
+        command: str = str(input("\nEnter command (q to quit, enter to calculate) :"))
 
-        if command.lower() == 'q':
-            print('Quitting now! Hope I helped!')
+        if command.lower() == "q":
+            print("Quitting now! Hope I helped!")
             break
-
-        elif command.lower() != '':
-            print('Please enter a valid command!')
+        elif command.lower() != "":
+            print("Please enter a valid command!")
             continue
-
         else:
             print(
-                '\nEnter funds here.\n'
-                'Note: If you want to check if your overall portfolio needs rebalancing, enter as\n'
-                'current value + funds being added OR as total account value')
+                "\nEnter funds here.\n"
+                "Note: If you want to check if your overall portfolio needs rebalancing, enter as\n"
+                "current value + funds being added OR as total account value"
+            )
             total: float = functions.get_input_total()
-            print(f'Total value = {total}')
-            
+            print(f"Total value = {total}")
+
             functions.show_output(percentage_weights, total)
-                    
-            
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
